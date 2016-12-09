beb8523642d521bf68601e5385b7007f8d7e97da
Joseph Dalrymple <Swivelgames@users.noreply.github.com>
2016-12-09 15:10:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 var fs = require('fs');
 
+var map = {
+    '(': "\uFF08",
+    ')': "\uFF09",
+    '"': "\uFF02",
+    "'": "\uFF07",
+    ";": "\u037E",
+    ",": "\u002C",
+    "=": "\uFF1D",
+    "+": "\uFE62",
+    "&": "\uFF06",
+    "{": "\uFF5B",
+    "}": "\uFF5D",
+    "[": "\uFF3B",
+    "]": "\uFF3D",
+    ">": "\uFF1E",
+    "<": "\uFF1C"
+};
+
 var files = process.argv.slice(2);
 
 files.forEach(function (file) {
     var contents = fs.readFileSync(file, 'utf8');
-    fs.writeFileSync(file, contents.replace(/;/g, ';'));
+    for(var chr in map) {
+        contents = contents.replace(chr, map[chr]);
+    }
+    fs.writeFileSync(file, contents);
 });
